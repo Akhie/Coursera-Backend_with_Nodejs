239d643142fab153bda67c560293ea9cc1e1df24
Akhie <Akhilesh.yadav200093@gmail.com>
2020-11-10 15:47:01 +0530

    Express Session Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -46,43 +46,29 @@ app.use(session({
   store: new FileStore()
 }));
 
-// Authorization
+// Routes don't need authentication
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Authenticate user
 function auth (req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {  
-        res.session.user = 'admin'; 
-        next(); // authorized & session setup 
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-    }
+if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
   }
   else {
-      if (req.session.user === 'admin') {
-        console.log('req.session: ',req.session);
-        next(); // already session stored
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
+}
 }
 
 app.use(auth);
@@ -90,8 +76,6 @@ app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
