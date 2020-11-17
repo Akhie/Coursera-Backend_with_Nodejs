675dbea762bc8b8cc4474b4b1794a79071630082
Akhie <Akhilesh.yadav200093@gmail.com>
2020-11-17 21:34:03 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://Akhilesh:Akhi1234@cluster0.6fyyy.mongodb.net/conFusion?retryWrites=true&w=majority'
+    'mongoUrl' : 'mongodb+srv://Akhilesh:Akhi1234@cluster0.6fyyy.mongodb.net/conFusion?retryWrites=true&w=majority',
+    'facebook': {
+        clientId: '1278260422552009',
+        clientSecret: '493c265a773eab53f2a664a5d3385ccf'
+    }
 }
