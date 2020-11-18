903d0fb078cd798ab9a64b380145304a9f45b96b
Akhie <Akhilesh.yadav200093@gmail.com>
2020-11-18 11:03:18 +0530

    Final Assignment

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,11 +16,13 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 //Models
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
+const Favorite = require('./models/favorite');
 
 // Connection with Database
 const mongoose = require('mongoose');
@@ -66,6 +68,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
